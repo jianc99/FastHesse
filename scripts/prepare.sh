285988ea34465a669df809d4887393ad97ac7414
jianc99 <jianchen0311@gmail.com>
2024-06-17 21:13:03 -0400

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,1 @@
-python scripts/download.py --repo_id $1 && python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/$1
+python scripts/download.py --repo_id $1 --hf_token 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 && python scripts/convert_hf_checkpoint.py --checkpoint_dir checkpoints/$1
