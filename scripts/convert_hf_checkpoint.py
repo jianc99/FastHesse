61ce70191bffaee925aa133e22485b62f8907de4
jianc99 <jianchen0311@gmail.com>
2024-06-17 21:24:13 -0400

    Update convert_hf_checkpoint.py

--- scripts/convert_hf_checkpoint.py
+++ scripts/convert_hf_checkpoint.py
@@ -3,6 +3,8 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+import sys
+sys.path.append("..")
 import json
 import re
 import shutil
